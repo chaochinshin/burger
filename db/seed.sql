86b297ffa3a4509ea719f8600ef266520ae08a34
unknown <chaochinshin@gmail.com>
2020-05-15 22:52:12 -0400

    first commit

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (burger_name) VALUES ('Hamburger');
+INSERT INTO burger (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burger (burger_name, devoured) VALUES ('Mushroomburger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('BigMac', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Baconburger', true);
+INSERT INTO burger (burger_name) VALUES ('Rodeoburger');
---
+++ public/assets/js/burger.js
@@ -0,0 +1,61 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".delburger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted id ", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+        burger: $("#burger").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burger", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".update-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var updatedBurger = {
+       burger: $("#burger").val().trim()
+    };
+
+    var id = $(this).data("id");
+
+    // Send the POST request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: updatedBurger
+    }).then(
+      function() {
+        console.log("updated burger");
+        // Reload the page to get the updated list
+        location.assign("/");
+      }
+    );
+  });
+});
